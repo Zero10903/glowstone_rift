24d79c65f0a29a899e1e44f99b9f3a2dd47fee8d
Alejandro Bustos <sam.alejandro.bustos@gmail.com>
2025-12-03 20:42:41 -0300

    feat(playground): added Health and Damage testing objects

---
+++ Assets/Scripts/Testing/TestHeal.cs
@@ -0,0 +1,17 @@
+using Interfaces;
+using UnityEngine;
+
+namespace Testing
+{
+    public class TestHeal : MonoBehaviour
+    {
+        [SerializeField] private float healAmount;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            IHealable healable = other.GetComponent<IHealable>();
+
+            healable?.Heal(healAmount);
+        }
+    }
+}
